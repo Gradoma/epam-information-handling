a86efb6677949720c477a0bc02860c4d565a9341
Gradoma <gradomski25@gmail.com>
2020-04-15 12:30:36 +0300

    logger and tests set

---
+++ src/main/java/by/epamtraining/information_handling_task/Runner.java
@@ -0,0 +1,12 @@
+package by.epamtraining.information_handling_task;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Runner {
+    private static Logger logger = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        logger.info("Hello!");
+    }
+}
