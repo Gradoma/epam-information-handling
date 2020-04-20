d7e43f0fb25670af43502ca44d8fc691a4004ed4
Gradoma <gradomski25@gmail.com>
2020-04-20 08:56:48 +0300

    paragraph parser upd

--- src/main/java/by/epamtraining/information_handling_task/parser/ParagraphParser.java
+++ src/main/java/by/epamtraining/information_handling_task/parser/ParagraphParser.java
@@ -1,20 +1,22 @@
 package by.epamtraining.information_handling_task.parser;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ParagraphParser extends AbstractParser {
-    private static final Pattern SENTENCE = Pattern.compile("\\.+[.?!…]");
+    private static final Pattern SENTENCE = Pattern.compile("\\s.+?[.?!…]");
 
     public ParagraphParser(){
         super(new SentenceParser());
     }
 
-
     @Override
     public void parse(String stringForParsing) {
         //
-        String s = stringForParsing + " parag ";
-        System.out.println(s);
-        parseNext(s);
+        Matcher matcher = SENTENCE.matcher(stringForParsing);
+        while (matcher.find()){
+            System.out.println(matcher.group());
+            parseNext(matcher.group());
+        }
     }
 }
