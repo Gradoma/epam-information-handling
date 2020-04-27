7886ef31583579178068903f585feb14f6bb0f16
Gradoma <gradomski25@gmail.com>
2020-04-27 12:18:17 +0300

    sorting fixed

--- src/main/java/by/epamtraining/iht/action/ParagSizeComparator.java
+++ src/main/java/by/epamtraining/iht/action/ParagSizeComparator.java
@@ -10,10 +10,6 @@ public class ParagSizeComparator implements Comparator<TextComposite> {
     public int compare(TextComposite o1, TextComposite o2) {
         int size1 = o1.getChildComponents().size();
         int size2 = o2.getChildComponents().size();
-//        System.out.println("size1=" + size1);
-//        System.out.println(o1.toString());
-//        System.out.println("size2=" + size2);
-//        System.out.println(o2.toString());
         return Integer.compare(size1, size2);
     }
 }
